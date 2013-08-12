c7f7bd8346d3fe7d9326cc3382363ff44ac4d5e3
Ankit Tyagi <ankit.tyagi@me.com>
2013-08-11 17:48:26 -0700

    gotta understand this db connection stuff eventually

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -3,11 +3,27 @@ package controllers
 import play.api.mvc.{Action, Controller}
 import play.api.data.Forms._
 import play.api.data._
+import models.AdminUser
 
 object LoginController extends Controller {
 
-  def login = Action {
-    Ok(views.html.admin.adminHome())
-  }
+  def login = TODO
+//  { implicit request =>
+//    loginForm.bindFromRequest.fold(
+//      errors =>
+//        BadRequest(views.html.homepage.index(), errors),
+//      (email, password) => {
+//        val adminUser = AdminUser.authenticate(username, password)
+//        Ok(views.html.admin.adminHome(adminUser))
+//      }
+//    )
+//  }
+
+  val loginForm = Form(
+    tuple(
+      "email" -> email,
+      "password" -> text
+    )
+  )
 
 }
--- app/models/AdminUser.scala
+++ app/models/AdminUser.scala
@@ -5,23 +5,29 @@ import play.api.db.DB
 import play.api.data.Forms._
 import play.api.data._
 import play.api.Play.current
+import play.api.libs.Crypto
 
-class AdminUser {
+case class AdminUser(username: String, password: String)
 
-  DB.withConnection { implicit c =>
-    val result: Boolean = SQL("SELECT 1").execute();
+object AdminUser {
 
+  val connection = DB.getConnection("default")
+
+  def authQuery(username: String, password: String): Sql = SQL(
+    "SELECT * " +
+    "FROM admin_user " +
+    "WHERE username = {username} " +
+      "AND password = {password}")
+        .on(
+          "username" -> username
+          ,"password" -> password
+      )
+
+  def authenticate(username: String, password: String): AdminUser = {
+      val signedPass = Crypto.sign(username.concat(password))
+      val adminUser = authQuery(username, signedPass).
+        apply()(connection)
+      adminUser.asInstanceOf[AdminUser]
   }
-
-  def authenticate() = {
-
-  }
-
-  val loginForm = Form(
-    tuple(
-      "email" -> text,
-      "password" -> text
-    )
-  )
 
 }
