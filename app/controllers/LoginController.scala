30189555e446bbf9c65b0c31597422dedd16b53e
Ankit Tyagi <atyagi@dandb.com>
2013-08-16 13:24:48 -0700

    testing more things, slick is confusing

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -3,17 +3,29 @@ package controllers
 import play.api.mvc.{Action, Controller}
 import play.api.data.Forms._
 import play.api.data._
+import play.api.Play.current
 import models.AdminUser
+import play.api.db.DB
+import scala.slick.jdbc.meta.MBestRowIdentifierColumn.Scope.Session
 
 object LoginController extends Controller {
 
-  def login = TODO
+  def login = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(),
+      adminUser => Ok(views.admin.adminHome)
+    )
+  }
 
   val loginForm = Form(
     tuple(
       "email" -> email,
       "password" -> text
-    )
+    ) verifying("Email/Password not valid", result => result match {
+      case (email, password) => AdminUser.authenticate(email, password)
+    })
   )
+
+
 
 }
